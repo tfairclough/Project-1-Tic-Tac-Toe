63510c0cb759ed53d9d72c4d2d71d2a1e18d86a7
Tom Fairclough <tom_fairclough@btinternet.com>
2023-01-06 09:29:59 +0000

    Psuedocode JS

---
+++ Codebase/js/main.js
@@ -0,0 +1,23 @@
+// Reset Game
+// Button with event listener to restart game
+    // Resets the board to orginal state
+    // Tracks and Alternates who goes first
+    // resets tiles that can be clicked
+
+
+
+// Reset Scores
+    // Button with event listener to reset the player scores
+    // Set wins/loses/ties to zero 
+
+
+// BoardGame
+    // Event listener on each tile 
+    // Alteternates what character is added to the tile
+    // Cannot click a till that is already clicked/contains a charcter until reset
+    // Once clicked add the charcter to innertext
+    // Once succcessfully clicked change turns
+    // Check win condition
+        // Check all possible three directions for same matches
+    // Update Response to indicate player turn
+    // If there have been 9 turns, declare tie
