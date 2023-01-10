b4ce9f3a8a94a52f21e08c65d5bb6dccd81873ac
Tom Fairclough <tom_fairclough@btinternet.com>
2023-01-10 11:09:58 +0000

    StartingPlayer Now Alternates

--- Codebase/js/main.js
+++ Codebase/js/main.js
@@ -6,25 +6,26 @@ const gameTiles = document.querySelectorAll(".gametile")
 const responseToPlayer = document.querySelector("#responseText")
 const tieCounter = document.querySelector(`#Tie`)
 const scoreCounters = document.querySelectorAll(".scoreCounter")
-const muteIcon = document.querySelector('#muteButton')
+const muteIcon = document.querySelector("#muteButton")
 
 // Variables 
 const winConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-let clickedTile = new Audio('../Audio/TilePlacement.mp3')
-let newGameSound = new Audio('../Audio/NewGameSound.mp3')
+let clickedTile = new Audio("../Audio/TilePlacement.mp3")
+let newGameSound = new Audio("../Audio/NewGameSound.mp3")
 
 // Event Listeners
-resetScoresButton.addEventListener('click', resetScores)
-newGameButton.addEventListener('click', newGame)
-gameTiles.forEach(tile => tile.addEventListener('click', tileClicked))
-muteIcon.addEventListener('click', toggleMute)
+window.addEventListener("load", selectStartingPlayer)
+resetScoresButton.addEventListener("click", resetScores)
+newGameButton.addEventListener("click", newGame)
+gameTiles.forEach(tile => tile.addEventListener("click", tileClicked))
+muteIcon.addEventListener("click", toggleMute)
 
 
 // Need to track player who went first and chnage it on new game
 function newGame() {
     playAudio(newGameSound)
     resetBoard()
-    updateResponse("")
+    selectStartingPlayer()
     newGameButton.classList.remove("buttonglow")
 }
 
@@ -39,36 +40,46 @@ function resetScores() {
 //3. Check the result of the turn
 //4. Change Player
 function tileClicked(e) {
-    if ((gameBoard.getAttribute("gamestate") === "on") && (e.target.innerHTML === '')) {
+    if ((gameBoard.getAttribute("gamestate") === "on") && (e.target.innerHTML === "")) {
         playAudio(clickedTile)
         updateTileValue(e)
         checkTurnResult()       
-        changePlayer()
     }
 }
 
 
 function checkTurnResult() {
    let gameState = currentGameState([...gameTiles].map(tile => tile.innerHTML))
-    if ((gameState === 'Winner') || (gameState === 'Tie')) {
-        toggleBoardInteraction('off')
-        updateScores(gameState)
+    if ((gameState === "Winner") || (gameState === "Tie")) {
+        endGame(gameState)
+    } else {
+        nextTurn()
     }
-    updateResponse(gameState)
 }
 
+
+function nextTurn() {
+    changePlayer('player')
+    updateResponse(`Player ${gameBoard.getAttribute("player")} turn`)
+}
+
+function endGame(gameState) {
+    toggleBoardInteraction("off")
+    updateScores(gameState)
+    updateResponse(gameState)
+}
 
 function currentGameState(currentGameBoard) {
     return winConditionMet(currentGameBoard) ? "Winner"
         : drawConditionMet(currentGameBoard) ? "Tie"
-            : `Player ${gameBoard.getAttribute('player')} turn`
+        : `Continue`
 }
 
 
 function updateScores(result) {
     if (result === "Winner") {
         displayWinVisual()
-        +document.querySelector(`#${gameBoard.getAttribute('player')}`).innerHTML++
+        +document.querySelector(`#${gameBoard.getAttribute("player")}`).innerHTML++
     } else {
         +tieCounter.innerHTML++
     }
@@ -82,13 +93,13 @@ function displayWinVisual() {
 
 
 function resetBoard() {
-    gameTiles.forEach(tile => {tile.innerHTML = ''; tile.classList.remove("noInteractions", "winGlow")})
-    toggleBoardInteraction('on')
+    gameTiles.forEach(tile => {tile.innerHTML = ""; tile.classList.remove("noInteractions", "winGlow")})
+    toggleBoardInteraction("on")
 }
 
 
 function drawConditionMet(currentGameBoard) {
-    return currentGameBoard.every((tile) => tile !== '')
+    return currentGameBoard.every((tile) => tile !== "")
 }
 
 
@@ -98,7 +109,7 @@ function winConditionMet(currentGameBoard) {
 
 
 function allEqualAndNotBlank(arr) {
-    return arr.every(val => val === arr[0] && val !== '');
+    return arr.every(val => val === arr[0] && val !== "");
 }
 
 
@@ -115,18 +126,17 @@ function updateTileValue(e) {
 
 function toggleBoardInteraction(status) {
     gameBoard.setAttribute("gamestate", `${status}`)
-    if (status=== 'off') {
+    if (status=== "off") {
         newGameButton.classList.add("buttonglow")
         gameTiles.forEach(tile => {tile.classList.add("noInteractions")})
     }
 }
 
-
-function changePlayer() {
-    if (gameBoard.getAttribute('player') === 'X') {
-        gameBoard.setAttribute("player", 'O')
+function changePlayer(attributeSelector) {
+    if (gameBoard.getAttribute(attributeSelector) === "X") {
+        gameBoard.setAttribute(attributeSelector, "O")
     } else {
-        gameBoard.setAttribute("player", 'X')
+        gameBoard.setAttribute(attributeSelector, "X")
     }
 }
 
@@ -135,11 +145,16 @@ function playAudio(sound) {
         sound.play()
 }
 
-
 function toggleMute(e) {
     muteIcon.classList.toggle("muted")
 }
 
+function selectStartingPlayer() {
+    let response = `Player ${gameBoard.getAttribute("startingPlayer")} turn`
+    gameBoard.setAttribute("player", gameBoard.getAttribute("startingPlayer"))
+    updateResponse(response)
+    changePlayer("startingPlayer")
+}
 
 
 
@@ -147,29 +162,3 @@ function toggleMute(e) {
 function log(input) {
     console.log(input)
 }
-
-
-
-// Reset Game
-// Button with event listener to restart game
-    // Resets the board to orginal state
-    // Tracks and Alternates who goes first
-    // resets tiles that can be clicked
-
-
-// Reset Scores
-    // Button with event listener to reset the player scores
-    // Set wins/loses/ties to zero 
-
-
-
-// BoardGame
-    // Event listener on each tile 
-    // Alteternates what character is added to the tile
-    // Cannot click a till that is already clicked/contains a charcter until reset
-    // Once clicked add the charcter to innertext
-    // Once succcessfully clicked change turns
-    // Check win condition
-        // Check all possible three directions for same matches
-    // Update Response to indicate player turn
-    // If there have been 9 turns and no winner, declare tie
